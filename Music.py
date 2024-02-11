fd3fe0eed14a3ddec76eb4c56dcd12ede34949f3
Erik van Zijst <erik.van.zijst@gmail.com>
2024-02-11 13:41:52 +0100

    Add youtube download option

--- Music.py
+++ Music.py
@@ -1,8 +1,10 @@
 import os
+import shutil
 import sys
 import time
 import sqlite3
 from subprocess import Popen, PIPE
+from tempfile import TemporaryDirectory
 from urllib.parse import urlencode, urlparse, urlunparse, quote
 
 import requests
@@ -14,7 +16,7 @@ st.set_page_config(layout="wide", page_title="Music search")
 root = '../../nuc/Music/spring2007' if not sys.argv[1:] else sys.argv[1]
 
 
-@st.cache_data(ttl=3600, show_spinner=False)
+@st.cache_data(show_spinner=False)
 def is_warm() -> float:
     return time.time()
 
@@ -34,7 +36,7 @@ def index(root: str) -> None:
             bar.progress(cnt / total, f'Indexing {root}')
             for filename, p in [(fn, os.path.join(dirp, fn)) for fn in filenames]:
                 if p not in existing:
-                    conn.execute('insert into songs (name, path) values (?, ?)', (filename, os.path.join(dirp, filename)))
+                    conn.execute('insert into songs (name, path) values (?, ?)', (filename, p))
                 existing.discard(p)
         if existing:
             bar.progress(1.0, f'Dropping {len(existing)} removed songs...')
@@ -76,24 +78,77 @@ def play(fn: str):
     requests.get(url).raise_for_status()
 
 
-if 'song' not in st.session_state:
-    st.session_state['song'] = None
+def player():
+    if 'song' not in st.session_state:
+        st.session_state['song'] = None
+
+    st.markdown('# Music search')
+    ensure_vlc()
+
+    with sqlite3.connect(DB) as conn:
+        if time.time() - is_warm() < .05:
+            index(root)
+        count = conn.execute("select count(*) from songs").fetchall()[0][0]
+
+        def fts(term: str):
+            return conn.execute('select name, path from songs where path match ? ORDER BY rank',
+                                (' OR '.join([t.replace('"', '""') + '*' for t in term.split()]),)).fetchall()
+
+        if (selected_value := st_searchbox(fts, key="searchbox",
+                                           label=f'{count} songs in {root}')) != st.session_state.song:
+            play(selected_value)
+            st.session_state.song = selected_value
+
+    st.markdown(f'<iframe src="http://:password@localhost:9000" allow="autoplay" style="width: 100%; overflow: hidden"></iframe>',
+                unsafe_allow_html=True)
 
 
-st.markdown('# Music search')
-ensure_vlc()
+def download():
+    # st.markdown('# Download songs')
 
-with sqlite3.connect(DB) as conn:
-    if time.time() - is_warm() < .05:
-        index(root)
-    count = conn.execute("select count(*) from songs").fetchall()[0][0]
+    if 'url' not in st.session_state:
+        st.session_state['url'] = None
+    if 'dl_log' not in st.session_state:
+        st.session_state['dl_log'] = ''
 
-    def fts(term: str):
-        return conn.execute('select name, path from songs where path match ? ORDER BY rank',
-                            (' OR '.join([t.replace('"', '""') + '*' for t in term.split()]),)).fetchall()
+    url = st.text_input('Paste a YouTube URL:', placeholder='https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+    placeholder = st.empty()
+    log = st.code(st.session_state.dl_log)
+    info = st.empty()
 
-    if (selected_value := st_searchbox(fts, key="searchbox", label=f'{count} songs in {root}')) != st.session_state.song:
-        play(selected_value)
-        st.session_state.song = selected_value
+    if url and url != st.session_state.url:
+        print(f'{url}')
+        info.empty()
+        st.session_state.dl_log = ''
+        with TemporaryDirectory() as tmpdir:
+            proc = Popen(['yt-dlp', '--extract-audio', '--format', 'bestaudio', '-x', '--audio-format', 'mp3', url],
+                         cwd=tmpdir, stdin=PIPE, stdout=PIPE)
+            try:
+                proc.stdin.close()
+                stdout: str = ''
+                with placeholder.container():
+                    with st.spinner():
+                        while line := proc.stdout.readline().decode('UTF-8'):
+                            log.text(stdout := (stdout + line))
+            finally:
+                with info:
+                    if retval := proc.wait():
+                        st.error(f'Download failed ({retval})')
+                    else:
+                        os.makedirs(os.path.join(root, 'youtube'), exist_ok=True)
+                        with sqlite3.connect(DB) as conn:
+                            for fn in os.listdir(tmpdir):
+                                dst = shutil.copy(os.path.join(tmpdir, fn), os.path.join(root, 'youtube'))
+                                conn.execute('insert into songs (name, path) values (?, ?)', (fn, dst))
+                        st.success(f'Song downloaded successfully and added to library!')
 
-st.markdown(f'<iframe src="http://:password@localhost:9000" style="width: 100%; overflow: hidden"></iframe>', unsafe_allow_html=True)
+        st.session_state.url = url
+
+
+page_names_to_funcs = {
+    "Player": player,
+    "Download": download,
+}
+
+selected_page = st.sidebar.selectbox('', options=page_names_to_funcs.keys())
+page_names_to_funcs[selected_page]()
