c7aa9e6e9005a82ee7b077fb1dc93a9787a8bec8
Erik van Zijst <erik.van.zijst@gmail.com>
2024-02-24 16:47:07 +0100

    Allow songs to be starred

--- Music.py
+++ Music.py
@@ -18,7 +18,7 @@ from streamlit_searchbox import st_searchbox
 
 DB = 'music.db'
 extensions = {'.mp3', '.flac', '.ogg', '.oga', '.mogg', '.opus', '.vox', '.webm', '.m4a', '.wav', '.wma', '.aac', '.aax', '.m4b'}
-st.set_page_config(layout="wide", page_title="Music search")
+st.set_page_config(layout="wide", page_title="🎵 Music")
 fs_root = '.' if not sys.argv[1:] else sys.argv[1]
 base_url = urlparse(st_javascript("await fetch('').then(r => window.parent.location.href)"))._replace(path='')
 ranking_sql = """
@@ -41,8 +41,9 @@ def is_warm() -> float:
     return time.time()
 
 
-def align(content: str, direction: Literal['right', 'center'], unsafe_allow_html=False):
-    st.markdown(f'<div style="text-align: {direction}; width: 100%">{content if unsafe_allow_html else html.escape(content)}</div>',
+def align(content: str, direction: Literal['right', 'center'], nowrap=False, unsafe_allow_html=False):
+    st.markdown(f'<div style="text-align: {direction}; width: 100%; {"white-space: nowrap;" if nowrap else ""}">'
+                f'{content if unsafe_allow_html else html.escape(content)}</div>',
                 unsafe_allow_html=True)
 
 
@@ -55,7 +56,8 @@ def index(root: str) -> None:
 
     with sqlite3.connect(DB) as conn:
         conn.execute('create virtual table if not exists songs using fts5(name, path)')
-        conn.execute('create table if not exists played (path varchar, at datetime default current_timestamp)')
+        conn.execute('create table if not exists played (path varchar not null, at datetime default current_timestamp)')
+        conn.execute('create table if not exists tags (path varchar not null, tag varchar not null, primary key (path, tag))')
         conn.execute('delete from songs')
 
         for idx, p in enumerate(paths, 1):
@@ -87,6 +89,38 @@ def get_most_played() -> pd.DataFrame:
         return pd.read_sql_query(f'{ranking_sql} order by rank, path limit 100', conn)[['Count', 'Song']]
 
 
+@st.cache_data(show_spinner=False)
+def get_starred() -> pd.DataFrame:
+    with sqlite3.connect(DB) as conn:
+        return pd.read_sql_query(f'''
+            select s.name as 'Song'
+            from tags t
+            join songs s on s.path = t.path
+            left join (select path, max(at) as at from played group by path) p on p.path = t.path
+            where t.tag = ?
+            order by p.at desc, t.path
+            ''', conn, params=['star'])
+
+
+def star(path: str):
+    with sqlite3.connect(DB) as conn:
+        conn.execute('insert into tags (path, tag) values (?, ?) on conflict do nothing' if st.session_state.star else
+                     'delete from tags where path = ? and tag = ?', [path, 'star'])
+        get_starred.clear()
+
+
+def stats():
+    with st.expander('Stats'):
+        last, star, most = st.columns(3)
+
+        last.write('🕒 Last played')
+        last.dataframe(get_last_played(), hide_index=True, use_container_width=True)
+        most.write('🏆 Most played')
+        most.dataframe(get_most_played(), hide_index=True, use_container_width=True)
+        star.write('⭐ Starred')
+        star.dataframe(get_starred(), hide_index=True, use_container_width=True)
+
+
 st.markdown("""
     <style>
         .reportview-container {
@@ -98,84 +132,87 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 
-if 'song' not in st.session_state:
-    st.session_state['song'] = None
+def player():
+    if 'song' not in st.session_state:
+        st.session_state['song'] = None
 
-with sqlite3.connect(DB) as conn:
-    if time.time() - is_warm() < .05:
-        index(fs_root)
-    count = conn.execute("select count(*) from songs").fetchall()[0][0]
+    with sqlite3.connect(DB) as conn:
+        if time.time() - is_warm() < .05:
+            index(fs_root)
+        count = conn.execute("select count(*) from songs").fetchall()[0][0]
 
-    def fts(term: str):
-        return conn.execute('select name, path from songs where path match ? ORDER BY rank',
-                            (' OR '.join([t.replace('"', '""') + '*' for t in term.split()]),)).fetchall()
+        def fts(term: str):
+            return conn.execute('select name, path from songs where path match ? ORDER BY rank',
+                                (' OR '.join([t.replace('"', '""') + '*' for t in term.split()]),)).fetchall()
 
-    with st.container(border=True):
-        if (selected_value := st_searchbox(fts, key="searchbox",
-                                           label=f'{count} songs')) != st.session_state.song:
-            conn.execute('insert into played (path) values (?)', (selected_value,))
-            st.session_state.song = selected_value
-            get_rank.clear()
-            get_last_played.clear()
-            get_most_played.clear()
+        with st.container(border=True):
+            if (selected_value := st_searchbox(fts, key="searchbox",
+                                               label=f'{count} songs')) != st.session_state.song:
+                conn.execute('insert into played (path) values (?)', (selected_value,))
+                st.session_state.song = selected_value
+                for f in [get_rank, get_last_played, get_most_played]:
+                    f.clear()
 
-        url = urlunparse(base_url._replace(path=quote(f'/music/{st.session_state.song}'))) if st.session_state.song else ''
-        st.markdown(f"""<audio id="player" controls autoplay="true" src="{url}" style="width: 100%;"></audio>""",
-                    unsafe_allow_html=True)
-        tag = st.session_state.song and eyed3.load(os.path.join(fs_root, st.session_state.song))
-        c1, c2 = st.columns([2, 1])
-        c1.markdown(os.path.basename(st.session_state.song) if st.session_state.song else '')
-        with c2:
-            align('' if tag is None else f'[🏆 #{get_rank(st.session_state.song)}] [{tag.info.sample_freq / 1000}kHz]',
-                  'right')
+            url = urlunparse(base_url._replace(path=quote(f'/music/{st.session_state.song}'))) if st.session_state.song else ''
+            st.markdown(f"""<audio id="player" controls autoplay="true" src="{url}" style="width: 100%;"></audio>""",
+                        unsafe_allow_html=True)
+            c1, c2 = st.columns([2, 1])
+            if st.session_state.song:
+                tag = eyed3.load(os.path.join(fs_root, st.session_state.song))
+                starred = conn.execute('select path from tags where path = ?', [st.session_state.song]).fetchone()
+                c1.toggle(f'[{"⭐" if starred else "✩"}]  {os.path.basename(st.session_state.song)}', key='star', value=bool(starred), on_change=star, args=(st.session_state.song,))
 
-with st.expander('Download'):
-    if 'dl_url' not in st.session_state:
-        st.session_state['dl_url'] = None
-    if 'dl_log' not in st.session_state:
-        st.session_state['dl_log'] = ''
-    if 'dl_status' not in st.session_state:
-        st.session_state['dl_status'] = lambda: st.empty()
+                with c2:
+                    align(f'[🏆 #{get_rank(st.session_state.song)}] [{(tag.info.sample_freq / 1000) if tag else "?"}kHz]',
+                      'right', nowrap=False)
+            else:
+                c1.markdown('')
 
-    dl_url = st.text_input('Paste a YouTube URL:', placeholder='https://www.youtube.com/watch?v=dQw4w9WgXcQ')
-    placeholder = st.empty()
-    log = st.code(st.session_state.dl_log, language='text')
 
-    if dl_url and dl_url != st.session_state.dl_url:
-        st.session_state.dl_log = ''
-        with TemporaryDirectory() as tmpdir:
-            proc = Popen(['yt-dlp', '--extract-audio', '--format', 'bestaudio', '-x', '-o', '%(title)s',
-                          '--audio-format', 'mp3', dl_url], cwd=tmpdir, stdin=PIPE, stdout=PIPE)
-            try:
-                proc.stdin.close()
-                with placeholder.container(), st.spinner():
-                    while line := proc.stdout.readline().replace(b'\r', b'\n').decode('UTF-8'):
-                        st.session_state.dl_log += line
-                        log.text(st.session_state.dl_log)
-            finally:
-                if retval := proc.wait():
-                    st.session_state.dl_status = lambda: st.error(f'Download failed ({retval})')
-                else:
-                    os.makedirs(os.path.join(fs_root, 'youtube'), exist_ok=True)
-                    with sqlite3.connect(DB) as conn:
-                        for fn in os.listdir(tmpdir):
-                            dst = shutil.copy(os.path.join(tmpdir, fn), os.path.join(fs_root, 'youtube'))
-                            dst = str(Path(dst).relative_to(fs_root))
-                            conn.execute('insert into songs (name, path) values (?, ?)', (fn, dst))
-                    msg = f"Song{'s' if len(os.listdir(tmpdir)) > 1 else ''} downloaded successfully and added to library!"
-                    st.session_state.dl_status = lambda: st.success(msg)
+def downloader():
+    with st.expander('Download'):
+        if 'dl_url' not in st.session_state:
+            st.session_state['dl_url'] = None
+        if 'dl_log' not in st.session_state:
+            st.session_state['dl_log'] = ''
+        if 'dl_status' not in st.session_state:
+            st.session_state['dl_status'] = lambda: st.empty()
 
-        st.session_state.dl_url = dl_url
-    st.session_state.dl_status()
+        dl_url = st.text_input('Paste a YouTube URL:', placeholder='https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+        placeholder = st.empty()
+        log = st.code(st.session_state.dl_log, language='text')
 
-with st.expander('Stats'), sqlite3.connect(DB) as conn:
-    c1, c2 = st.columns(2)
+        if dl_url and dl_url != st.session_state.dl_url:
+            st.session_state.dl_log = ''
+            with TemporaryDirectory() as tmpdir:
+                proc = Popen(['yt-dlp', '--extract-audio', '--format', 'bestaudio', '-x', '-o', '%(title)s',
+                              '--audio-format', 'mp3', dl_url], cwd=tmpdir, stdin=PIPE, stdout=PIPE)
+                try:
+                    proc.stdin.close()
+                    with placeholder.container(), st.spinner():
+                        while line := proc.stdout.readline().replace(b'\r', b'\n').decode('UTF-8'):
+                            st.session_state.dl_log += line
+                            log.text(st.session_state.dl_log)
+                finally:
+                    if retval := proc.wait():
+                        st.session_state.dl_status = lambda: st.error(f'Download failed ({retval})')
+                    else:
+                        os.makedirs(os.path.join(fs_root, 'youtube'), exist_ok=True)
+                        with sqlite3.connect(DB) as conn:
+                            for fn in os.listdir(tmpdir):
+                                dst = shutil.copy(os.path.join(tmpdir, fn), os.path.join(fs_root, 'youtube'))
+                                dst = str(Path(dst).relative_to(fs_root))
+                                conn.execute('insert into songs (name, path) values (?, ?)', (fn, dst))
+                        msg = f"Song{'s' if len(os.listdir(tmpdir)) > 1 else ''} downloaded successfully and added to library!"
+                        st.session_state.dl_status = lambda: st.success(msg)
 
-    c1.markdown('Last played')
-    c1.dataframe(get_last_played(), hide_index=True, use_container_width=True)
+            st.session_state.dl_url = dl_url
+        st.session_state.dl_status()
 
-    c2.markdown('Most played')
-    c2.dataframe(get_most_played(), hide_index=True, use_container_width=True)
+
+player()
+downloader()
+stats()
 
 align('<a href="https://github.com/erikvanzijst/music">'
       '<img src="https://badgen.net/static/github/code?icon=github">'
